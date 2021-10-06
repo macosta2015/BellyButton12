1d7e637e836c98ec32f38d94473347062e638b98
macosta2015 <macosta2015@my.fit.edu>
2021-10-05 21:02:04 -0400

    UPDATE

--- data-12-1-2-resources/plots.js
+++ data-12-1-2-resources/plots.js
@@ -1,9 +1,49 @@
 //Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
-Plotly.newPlot("plotArea", [{x: [5,10,15], y: [10,20,30]}]);
+//Plotly.newPlot("plotArea", [{x: [5,10,15], y: [10,20,30]}]);
 
+// var trace = {
+//     x: ["burrito", "pizza", "chicken"],
+//     y: [10, 18, 5],
+//     type: "bar"
+//  };
+//  Plotly.newPlot("plotArea", [trace]);
+
+
+//  var trace = [{
+//     x: ["burrito", "pizza", "chicken"],
+//     y: [10, 18, 5],
+//     type: "bar"
+// }];
+
+// Plotly.newPlot("plotArea", trace);
+
+
+///CODE FOR A GOOD CHART SAMPLE
 var trace = {
     x: ["burrito", "pizza", "chicken"],
     y: [10, 18, 5],
     type: "bar"
- };
- Plotly.newPlot("plotArea", [trace]);
+};
+
+var layout = {
+    title: "Luncheon Survey",
+    xaxis: {title: "Food Option"},
+    yaxis: {title: "Number of Respondents"}
+};
+
+Plotly.newPlot("plotArea", [trace], layout);
+
+
+///CODE FOR ANOTHER CHART WITH NON-ALCOHOLIC DATA
+var trace = {
+    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: "bar"
+   };
+   var data = [trace];
+   var layout = {
+    title: "'Bar' Chart",
+    xaxis: { title: "Drinks"},
+    yaxis: { title: "% of Drinks Ordered"}
+   };
+   Plotly.newPlot("plotArea", data, layout);
