9010d29a3a520274abe49c6d76f4ac12a3c46705
macosta2015 <macosta2015@my.fit.edu>
2021-10-05 20:52:32 -0400

    Uploading the chart

--- data-12-1-2-resources/plots.js
+++ data-12-1-2-resources/plots.js
@@ -1,2 +1,9 @@
 //Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
 Plotly.newPlot("plotArea", [{x: [5,10,15], y: [10,20,30]}]);
+
+var trace = {
+    x: ["burrito", "pizza", "chicken"],
+    y: [10, 18, 5],
+    type: "bar"
+ };
+ Plotly.newPlot("plotArea", [trace]);
