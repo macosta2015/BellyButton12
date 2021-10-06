92eed046aab03f77b23dbeb2d1ac754164e826b0
macosta2015 <macosta2015@my.fit.edu>
2021-10-06 19:56:08 -0400

    Uploading class work

--- data-12-1-2-resources/plots.js
+++ data-12-1-2-resources/plots.js
@@ -47,3 +47,28 @@ var trace = {
     yaxis: { title: "% of Drinks Ordered"}
    };
    Plotly.newPlot("plotArea", data, layout);
+
+
+///PIE CHART
+var trace = {
+    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: 'pie'
+   };
+   var data = [trace];
+   var layout = {
+    title: "'Pie' Chart",
+   };
+   Plotly.newPlot("plotArea", data, layout);
+
+
+///It is needed to be ran in the console
+var numbers = [1,2,3,4,5];
+//We have an anonymous function called num
+//It could had been anything else
+var doubled = numbers.map(function(num){
+    return num + 5;
+});
+console.log(doubled);
+
