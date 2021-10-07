3d1d80b8dd0d8318028127c16bdae8fe9ebacbd9
macosta2015 <macosta2015@my.fit.edu>
2021-10-06 23:13:36 -0400

    Finished class work
    Will start with the Challenge now

--- plots.js
+++ plots.js
@@ -1,97 +1,168 @@
-//Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
-//Plotly.newPlot("plotArea", [{x: [5,10,15], y: [10,20,30]}]);
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
+  
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text(result.location);
+    });
+  }
 
+
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+function init() {
+    data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }];
+    Plotly.newPlot("plot", data);
+  };
+  
+  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+  function updatePlotly() {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var dataset = dropdownMenu.property("value");
+  
+    var xData = [1, 2, 3, 4, 5];
+    var yData = [];
+  
+    if (dataset === 'dataset1') {
+      yData = [1, 2, 4, 8, 16];
+    };
+  
+    if (dataset === 'dataset2') {
+      yData = [1, 10, 100, 1000, 10000];
+    };
+  
+    var trace = {
+      x: [xData],
+      y: [yData],
+    };
+    Plotly.restyle("plot", trace);
+  };
+  
+  init();
+
+
+// FIRST PART OF THE CLASS
+
+
+
+// //Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+// //Plotly.newPlot("plotArea", [{x: [5,10,15], y: [10,20,30]}]);
+
+// // var trace = {
+// //     x: ["burrito", "pizza", "chicken"],
+// //     y: [10, 18, 5],
+// //     type: "bar"
+// //  };
+// //  Plotly.newPlot("plotArea", [trace]);
+
+
+// //  var trace = [{
+// //     x: ["burrito", "pizza", "chicken"],
+// //     y: [10, 18, 5],
+// //     type: "bar"
+// // }];
+
+// // Plotly.newPlot("plotArea", trace);
+
+
+// ///CODE FOR A GOOD CHART SAMPLE
 // var trace = {
 //     x: ["burrito", "pizza", "chicken"],
 //     y: [10, 18, 5],
 //     type: "bar"
-//  };
-//  Plotly.newPlot("plotArea", [trace]);
+// };
+
+// var layout = {
+//     title: "Luncheon Survey",
+//     xaxis: {title: "Food Option"},
+//     yaxis: {title: "Number of Respondents"}
+// };
+
+// Plotly.newPlot("plotArea", [trace], layout);
 
 
-//  var trace = [{
-//     x: ["burrito", "pizza", "chicken"],
-//     y: [10, 18, 5],
+// ///CODE FOR ANOTHER CHART WITH NON-ALCOHOLIC DATA
+// var trace = {
+//     x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+//     y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
 //     type: "bar"
-// }];
-
-// Plotly.newPlot("plotArea", trace);
-
-
-///CODE FOR A GOOD CHART SAMPLE
-var trace = {
-    x: ["burrito", "pizza", "chicken"],
-    y: [10, 18, 5],
-    type: "bar"
-};
-
-var layout = {
-    title: "Luncheon Survey",
-    xaxis: {title: "Food Option"},
-    yaxis: {title: "Number of Respondents"}
-};
-
-Plotly.newPlot("plotArea", [trace], layout);
+//    };
+//    var data = [trace];
+//    var layout = {
+//     title: "'Bar' Chart",
+//     xaxis: { title: "Drinks"},
+//     yaxis: { title: "% of Drinks Ordered"}
+//    };
+//    Plotly.newPlot("plotArea", data, layout);
 
 
-///CODE FOR ANOTHER CHART WITH NON-ALCOHOLIC DATA
-var trace = {
-    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: "bar"
-   };
-   var data = [trace];
-   var layout = {
-    title: "'Bar' Chart",
-    xaxis: { title: "Drinks"},
-    yaxis: { title: "% of Drinks Ordered"}
-   };
-   Plotly.newPlot("plotArea", data, layout);
+// ///PIE CHART
+// var trace = {
+//     labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+//     "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+//     values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+//     type: 'pie'
+//    };
+//    var data = [trace];
+//    var layout = {
+//     title: "'Pie' Chart",
+//    };
+//    Plotly.newPlot("plotArea", data, layout);
 
 
-///PIE CHART
-var trace = {
-    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
-    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: 'pie'
-   };
-   var data = [trace];
-   var layout = {
-    title: "'Pie' Chart",
-   };
-   Plotly.newPlot("plotArea", data, layout);
+// ///It is needed to be ran in the console
+// var numbers = [1,2,3,4,5];
+// //We have an anonymous function called num
+// //It could had been anything else
+// var doubled = numbers.map(function(num){
+//     return num + 5;
+// });
+// console.log(doubled);
 
 
-///It is needed to be ran in the console
-var numbers = [1,2,3,4,5];
-//We have an anonymous function called num
-//It could had been anything else
-var doubled = numbers.map(function(num){
-    return num + 5;
-});
-console.log(doubled);
+// //////////////////////
+// var integers = [0,1,2,3,4,5];
+// var slice1 = integers.slice(0,2);
 
+// var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
+// words.slice(3, );
 
-//////////////////////
-var integers = [0,1,2,3,4,5];
-var slice1 = integers.slice(0,2);
+// var topFiveCityNames = topFiveCities.map(city => city.City);
+// var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
 
-var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
-words.slice(3, );
-
-var topFiveCityNames = topFiveCities.map(city => city.City);
-var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
-
-var trace = {
-    x: topFiveCityNames,
-    y: topFiveCityGrowths,
-    type: "bar"
-  };
-  var data = [trace];
-  var layout = {
-    title: "Most Rapidly Growing Cities",
-    xaxis: { title: "City" },
-    yaxis: { title: "Population Growth, 2016-2017"}
-  };
-  Plotly.newPlot("bar-plot", data, layout);
+// var trace = {
+//     x: topFiveCityNames,
+//     y: topFiveCityGrowths,
+//     type: "bar"
+//   };
+//   var data = [trace];
+//   var layout = {
+//     title: "Most Rapidly Growing Cities",
+//     xaxis: { title: "City" },
+//     yaxis: { title: "Population Growth, 2016-2017"}
+//   };
+//   Plotly.newPlot("bar-plot", data, layout);
